765a4b7cae2ecd6a9078fa6e4b87769201f41b5e
hitbotK <121420138+hitbotK@users.noreply.github.com>
2025-06-03 12:20:37 +0700

    Add files via upload

---
+++ manga-site/script.js
@@ -0,0 +1,12 @@
+fetch('data/manga.json')
+  .then(res => res.json())
+  .then(data => {
+    const list = document.getElementById('manga-list');
+    data.manga.forEach(m => {
+      const a = document.createElement('a');
+      a.href = `manga.html?title=${m.slug}`; // ใช้ slug
+      a.textContent = m.title;
+      list.appendChild(a);
+      list.appendChild(document.createElement('br'));
+    });
+  });
